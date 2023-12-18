2a2b6778d8f1dcadb98c99884e063343ba7aa8a1
cmos_LEE <leege0610@cmosmedia.com>
2023-12-18 17:28:43 +0900

    first commit

---
+++ rknn/rknn_api/examples/rknn_ssd_demo/build/CMakeFiles/rknn_ssd_demo.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cmos/RV1126_RV1109_SDK/external/rknpu/rknn/rknn_api/examples/rknn_ssd_demo/src/main.cc" "/home/cmos/RV1126_RV1109_SDK/external/rknpu/rknn/rknn_api/examples/rknn_ssd_demo/build/CMakeFiles/rknn_ssd_demo.dir/src/main.cc.o"
+  "/home/cmos/RV1126_RV1109_SDK/external/rknpu/rknn/rknn_api/examples/rknn_ssd_demo/src/ssd.cc" "/home/cmos/RV1126_RV1109_SDK/external/rknpu/rknn/rknn_api/examples/rknn_ssd_demo/build/CMakeFiles/rknn_ssd_demo.dir/src/ssd.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../../librknn_api/include"
+  "../../3rdparty"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
